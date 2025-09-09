dd44fb7e384b74be853785241628aef9c1dd3ed2
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-09-09 13:37:21 +0200

    Improve version usage

--- reproduce/1-install.sh
+++ reproduce/1-install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
-# Creates a virtual conda environment and installs dependencies
 
+# (Re)create conda environment
+echo "Creating conda environment..."
 module --latest load Miniconda3
-conda create -n delphi python=3.11
+conda remove --name delphi --all --yes
+conda create --name delphi python=3.11 --yes
+conda init bash
+source ~/.bashrc
 conda activate delphi
-pip install -r requirements.txt
+
+# Install dependencies
+echo "Installing dependencies..."
+module load Python/3.11.3-GCCcore-12.3.0 # match python version in conda env
+pip install --requirement requirements.txt --force-reinstall
--- reproduce/2-train.sh
+++ reproduce/2-train.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
-# Train the model
 
-module --latest load Python
+# Reactivate conda environment
+module --latest load Miniconda3
+conda init bash
+source ~/.bashrc
+conda activate delphi
+
+# Train the model
+module load Python/3.11.3-GCCcore-12.3.0 # match python version in conda env
 python train.py config/train_delphi_demo.py --device=cuda --out_dir=Delphi-2M
