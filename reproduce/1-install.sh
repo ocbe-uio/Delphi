d3647f2dfee3a3cf3d4de89eb2540810635d29fb
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-09-09 10:59:13 +0200

    Add conda loading to install script

--- reproduce/1-install.sh
+++ reproduce/1-install.sh
@@ -1,4 +1,6 @@
 # Creates a virtual conda environment and installs dependencies
+
+module --latest load Miniconda3
 conda create -n delphi python=3.11
 conda activate delphi
 pip install -r requirements.txt
