5b0b60e09bc0172dd6b2f25fa2e300b57bf8aa72
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-09-09 10:50:00 +0200

    Add Bash scripts to install and train

---
+++ reproduce/1-install.sh
@@ -0,0 +1,4 @@
+# Creates a virtual conda environment and installs dependencies
+conda create -n delphi python=3.11
+conda activate delphi
+pip install -r requirements.txt
---
+++ reproduce/2-train.sh
@@ -0,0 +1,2 @@
+# Train
+python train.py config/train_delphi_demo.py --device=cuda --out_dir=Delphi-2M
