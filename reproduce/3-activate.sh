5526967a7b8dee4b6c2b789fb9ac2837c7a86391
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-09-09 13:56:10 +0200

    Fix duplicate file

--- reproduce/3-activate.sh
+++ reproduce/3-activate.sh
@@ -7,5 +7,5 @@ source ~/.bashrc
 conda activate delphi
 module load Python/3.11.3-GCCcore-12.3.0 # match python version in conda env
 
-echo -e "\nPlease run the following command to enter the conda environment:\n"
+echo -e "\nPlease run the following command to enter the work environment:\n"
 echo -e "conda activate delphi\n"
